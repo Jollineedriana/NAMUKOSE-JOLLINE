b55d802975af231ae8eb083a00e07f123caa7e63
Jollineedriana <142318556+Jollineedriana@users.noreply.github.com>
2023-12-07 12:32:43 +0300

    git

---
+++ question_four.py
@@ -0,0 +1,27 @@
+
+#(ii)
+def factorialOfNumber(n):
+    factorial= n*1*2*3*4*n
+    print("The factorial is ",factorial)
+factorialOfNumber(5)  
+
+#(iii)
+def sumOfNumbers(a,b):
+    sum=a+b
+    print("The sum is",sum)
+sumOfNumbers(3,4) 
+
+
+#(iv)   
+class Car:
+    def __init__(self,brand,year):
+        self.brand=brand
+        self.year=year
+        
+ #(v)       
+Car_one=Car("Lexus",2018)   
+print(Car_one.brand ,Car_one.year)
+
+ 
+
+
